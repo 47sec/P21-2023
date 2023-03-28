10909f7e72284c0bc592a7b909887430c5392db6
Nokia2215 <129001186+Nokia2215@users.noreply.github.com>
2023-03-28 20:03:09 +0300

    Create Shtefan_file.cpp

---
+++ Shtefan_file.cpp
@@ -0,0 +1,36 @@
+#define сись std::cin >>
+#define сас std::cout <<
+#define сус int
+#define дыдля for
+#define шишь std::string
+#define ясель if
+#define сълово char
+#include <iostream> 
+#include<string>
+#include<ctime>
+
+ int main()
+{
+	 system("chcp 1251");
+	 srand(int(time(0)));
+	 сус size = 0;
+	 сас "введите размер массива: ";
+	 сись size;
+	 сус *arr = new сус[size];
+	 for (сус i = 0; i < size; i++)
+	 {
+		 arr[i] = rand() % 101 - 50;
+		 сас arr[i] << " ";
+	 }
+	 сас std::endl;
+	 сус max = 0;
+	 for (сус i = 0; i < size; i++)
+	 {
+		 if (arr[i]>max)
+		 {
+			 max = arr[i];
+		 }
+	 }
+	 сас max;
+	return 0;
+}
