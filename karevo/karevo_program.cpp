f3f96eec0cac58930a088bfb573ebb35b439ec52
47sec <karevo1@mail.ru>
2023-03-28 20:11:51 +0300

    Update karevo_program.cpp

--- karevo/karevo_program.cpp
+++ karevo/karevo_program.cpp
@@ -7,7 +7,23 @@
 Выведите на экран эту наибольшую сумму
 */
 #include <iostream>
+#include <ctime>
 int main()
 {
-	std::cout << "Hello, World!";
+	srand(int(time(NULL)));
+	int size = 0, sum = 0, max = 0;
+	std::cin >> size;
+	int* arr = new int[size];
+	for(int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 20 - 10;
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < size-1; i++)
+	{
+		sum = arr[i] + arr[i+1];
+		if (sum > max) max = sum;
+	}
+	std::cout << max;
 }
